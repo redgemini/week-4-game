9e428be520294e8c5ba99a449920e763705d2917
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-02-20 15:21:22 -0600

    styleCSSAndDebugJS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,21 +7,63 @@
 //When win/loss - restart game with new random number, crystal values hidden, and updated wins and losses
 
 
-//Variables
+//-----Variables-----
 
 //Crystal Scores
-var purpleCrystal = 0;
-var blueCrystal = 0;
-var whiteCrystal = 0;
-var greenCrystal = 0;
+var purpleCrystal   = 0;
+var blueCrystal     = 0;
+var whiteCrystal    = 0;
+var greenCrystal    = 0;
 
 //Score
-var yourScore = 0
-var gameScore = 0
+var yourScore   = 0;
+var gameScore   = 0;
 
 //Wins/Losses
-var winCounter = 0;
+var winCounter  = 0;
 var lossCounter = 0;
+
+//-----Functions-----
+
+//Set Random number gererator function for game score and crystals
+
+var randomNum = function ( min,max){
+    return Math.floor ( Math.random () * (max - min + 1)) + min;
+}
+
+//--Function Start Game--
+
+function startGame(){
+
+//--Function Reset--
+
+//Set yourScore to 0
+var yourScore   = 0;
+
+//Set to number between (19-120)
+gameScore       = randomNum(19,120);
+
+//set crystals to number between (1-12)              
+purpleCrystal   = randomNum(1,12);
+blueCrystal     = randomNum(1,12);
+whiteCrystal    = randomNum(1,12);
+greenCrystal    = randomNum(1,12);
+
+
+//console.log RandomNum
+console.log("test");
+console.log(yourScore);
+console.log(purpleCrystal + blueCrystal + whiteCrystal + greenCrystal);
+}
+
+//HTML
+
+
+
+
+
+
+
 
 //Crystal Clicks
 $("#purple") .click(function(){
@@ -40,8 +82,19 @@ $("#green") .click(function(){
     alert("testgreen");
 });
 
+
+
+
+
+
+
+
+
 //GameScore
 
-//Process
+//-----Process-----
+startGame(); {
 
+
+}
 
