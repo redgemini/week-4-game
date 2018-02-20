f0d79086e00552a6e5be25347367413b3ad2bfe1
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-02-20 09:31:17 -0600

    addJSClicks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,5 +7,41 @@
 //When win/loss - restart game with new random number, crystal values hidden, and updated wins and losses
 
 
-//global variables
-console.log ("test");
+//Variables
+
+//Crystal Scores
+var purpleCrystal = 0;
+var blueCrystal = 0;
+var whiteCrystal = 0;
+var greenCrystal = 0;
+
+//Score
+var yourScore = 0
+var gameScore = 0
+
+//Wins/Losses
+var winCounter = 0;
+var lossCounter = 0;
+
+//Crystal Clicks
+$("#purple") .click(function(){
+    alert("testpurple");
+});
+
+$("#blue") .click(function(){
+    alert("testblue");
+});
+
+$("#white") .click(function(){
+    alert("testwhite");
+});
+
+$("#green") .click(function(){
+    alert("testgreen");
+});
+
+//GameScore
+
+//Process
+
+
