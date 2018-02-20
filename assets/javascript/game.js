680bbd0c5f6416ac6bf73d2ef8d4e8f6a9864c0f
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-02-20 01:25:51 -0600

    setupJS

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Pseudocoding for Crystal Collector
+//Game Rules: Guess number that add up to the random number shown
+//Need 4 Crystals with a value between 1-12
+//Everytime clicked points are added to total score/amount revealed onClick
+//Need random number generator for Game Start - number should be between 19-120
+//Player wins if player total matched random number
+//When win/loss - restart game with new random number, crystal values hidden, and updated wins and losses
+
+
+//global variables
+console.log ("test");
